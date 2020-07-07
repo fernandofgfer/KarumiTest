eb3bf8b7fbded94499c5fa452dc2b727b82cbedb
Fernando <fernando.fgfer@gmail.com>
2020-07-07 16:32:35 +0200

    Project added

---
+++ KarumiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KarumiTest
+//
+//  Created by Fernando Garcia Fernandez on 07/07/2020.
+//  Copyright © 2020 Fernando Garcia Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
