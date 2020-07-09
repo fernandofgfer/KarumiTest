87784f846b70b4adda925b7d05faccf2d7e04e82
Fernando <fernando.fgfer@gmail.com>
2020-07-09 14:00:28 +0200

    Login structure added

---
+++ KarumiTest/Login/Interactor/LoginInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  LoginInteractor.swift
+//  KarumiTest
+//
+//  Created by Fernando Garcia Fernandez on 08/07/2020.
+//  Copyright © 2020 Fernando Garcia Fernandez. All rights reserved.
+//
+
+import Foundation
