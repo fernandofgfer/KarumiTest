68f0854103047a6189571d69445404474d255c65
Fernando <fernando.fgfer@gmail.com>
2020-07-09 15:23:28 +0200

    View finished

--- KarumiTest/Modules/Login/View/LoginViewController.swift
+++ KarumiTest/Modules/Login/View/LoginViewController.swift
@@ -11,10 +11,38 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var continueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
     }
     
+    private func setUpView() {
+        setStyle()
+        setLocalizable()
+    }
+    
+    private func setStyle() {
+        continueButton.backgroundColor = .blue
+        continueButton.layer.cornerRadius = 5
+        continueButton.setTitleColor(.white, for: .normal)
+        
+        userNameTextField.layer.cornerRadius = 5
+        passwordTextField.layer.cornerRadius = 5
+    }
+    
+    private func setLocalizable() {
+        continueButton.setTitle(NSLocalizedString("continue.title", comment: ""), for: .normal)
+        userNameTextField.placeholder = NSLocalizedString("userName.placeholder", comment: "")
+        passwordTextField.placeholder = NSLocalizedString("password.placeholder", comment: "")
+    }
+    
+    @IBAction func continueButtonTapped(sender: UIButton) {
+     
+    }
 }
 
 extension LoginViewController: LoginViewProtocol {}
