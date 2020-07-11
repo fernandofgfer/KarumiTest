1b84da9aeff87f5c240846f2d3b8f70a508ebcf8
Fernando <fernando.fgfer@gmail.com>
2020-07-11 11:45:47 +0200

    Router communication with the presenter

---
+++ KarumiTestTests/Modules/Login/LoginPresenterTests.swift
@@ -0,0 +1,35 @@
+//
+//  LoginPresenterTests.swift
+//  KarumiTestTests
+//
+//  Created by Fernando Garcia Fernandez on 11/07/2020.
+//  Copyright © 2020 Fernando Garcia Fernandez. All rights reserved.
+//
+
+import XCTest
+
+class LoginPresenterTests: XCTestCase {
+
+    
+    override func setUpWithError() throws {
+        super.setUp()
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
